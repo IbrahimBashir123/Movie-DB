23fbe2450ed421ecfd02e834df4bd18eabab7618
Ibrahim Bachir <Ibrahimbashir11111@gmail.com>
2022-12-27 22:55:05 +0200

    Fixing Step 3

--- index.js
+++ index.js
@@ -5,6 +5,21 @@ const port = 3000;
 
 app.get("/", (req, res) => res.send("ok"));
 app.get("/test", (req, res) => res.send(`{status:200, message:"ok"}`));
-app.get("/time", (req, res) => res.send(`{status:200, message:<20:23>}`));
-app.listen(port, () => {  console.log("server is at http://localhost:3000");
+app.get("/time", (req, res) => {
+  let date_Timing = new Date();
+
+  let hours = date_Timing.getHours();
+  let minutes = date_Timing.getMinutes();
+  let dateTime = hours + ":" + minutes;
+
+  const response = {
+    status: 200,
+    message: dateTime,
+  };
+
+  res.send(response);
+});
+
+app.listen(port, () => {
+  console.log("server is at http://localhost:3000");
 });
