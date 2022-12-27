2c5a01e13fb298c45d2271445e61e7eb3b02ea02
Ibrahim Bachir <Ibrahimbashir11111@gmail.com>
2022-12-27 20:23:42 +0200

    Step 3 - Create an express simple API

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+const { response } = require("express");
 const express = require("express");
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => res.send("ok"));
-app.listen(port, () => {
-  console.log("server is at http://localhost:3000");
+app.get("/test", (req, res) => res.send(`{status:200, message:"ok"}`));
+app.get("/time", (req, res) => res.send(`{status:200, message:<20:23>}`));
+app.listen(port, () => {  console.log("server is at http://localhost:3000");
 });
